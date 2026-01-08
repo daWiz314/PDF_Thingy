75199d009b38e07789ae2cf7aade0e05fbcc073e
Harry <programmer6299@icloud.com>
2026-01-08 09:07:59 -0500

    Added comments explaining variables Attempted to add better memory management, by using actual disk not just ram Added pre-scan to give full page count Updated message displayed during scan to show page count Added error catching, although it shouldn't run due to PDFBox doing its own error catching Added additional unbindProgress() calls, to ensure proper unbinding.

--- PDF_Extro/src/com/dawiz314/PDF_Tool/Main.java
+++ PDF_Extro/src/com/dawiz314/PDF_Tool/Main.java
@@ -4,6 +4,7 @@ import PDF_Ext.classes.SearchResult;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Set;
 import java.util.function.Consumer;
 
@@ -15,13 +16,16 @@ import javafx.scene.control.ProgressBar;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
+import org.apache.pdfbox.io.IOUtils;
 
 public class Main {
-
-    static public ProgressBar myProgressBar;
-    static public ProgressBar overallProgress;
-    static public Label statusLabel;
-    static public Label overallStatusLabel;
+    // UI Elements for updating progress from the search task
+    static public ProgressBar myProgressBar;    // Top progress bar - Gives incremental progress of current file
+    static public ProgressBar overallProgress;  // Bottom ProgressBar - Gives incremental progress of overall search - BOUND TO TASK
+    static public Label statusLabel;            // Top status label - Gives current page being scanned
+    static public Label overallStatusLabel;     // Bottom status label - Gives overall search status                 - BOUND TO TASK
     static public Button searchStop;
 
     static Set<File> selectedFiles;
@@ -45,53 +49,100 @@ public class Main {
     }
 
     public static void attemptSearch() {
+        System.setProperty("org.apache.pdfbox.rendering.UsePureJavaCMYKConversion", "true");
+        MemoryUsageSetting memSetting = MemoryUsageSetting.setupMixed(512 * 1024 * 1024);
         if (selectedFiles == null || selectedFiles.isEmpty()) {
             overallStatusLabel.setText("Please select files first!");
-        return;
+            return;
         }
+
         activeSearchTask = new Task<>() {
             @Override protected Void call() throws Exception {
                 int fileCount = 0;
                 int totalFiles = selectedFiles.size();
+                int pageCount = 0;
+                int totalOverallPages = 0;
 
+                javafx.application.Platform.runLater(() -> {
+                    statusLabel.setText("Starting pre-scan...");
+                });
+
+                for (File file : selectedFiles) {
+                    statusLabel.setText("Starting pre-scan...");
+                    // This lambda tells PDFBox to create a temporary file cache for this document
+                    try (PDDocument doc = Loader.loadPDF(file, IOUtils.createTempFileOnlyStreamCache())) {
+                        totalOverallPages += doc.getNumberOfPages();
+                        updateProgress(fileCount, totalFiles);
+                        updateMessage("Pre-scanning file " + (fileCount + 1) + " of " + totalFiles + ": " + file.getName() + "\nPages scanned: " + totalOverallPages);
+                        doc.close();
+                        fileCount++;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                fileCount = 0;
                 for (File file : selectedFiles) {
                     if (isCancelled()) break;
 
                     fileCount++;
+                    System.err.println("Scanning file " + fileCount + " of " + totalFiles + ": " + file.getName());
 
                     updateProgress(fileCount, totalFiles);
-                    updateMessage("Scanning file " + fileCount + " of " + totalFiles + ": " + file.getName());
+                    updateMessage("Scanning file " + fileCount + " of " + totalFiles + ": " + file.getName() + "\nPages scanned: " + pageCount + " / " + totalOverallPages);
 
-                    try (PDDocument doc = Loader.loadPDF(file)) {
+                    try (PDDocument doc = Loader.loadPDF(file, IOUtils.createMemoryOnlyStreamCache())) {
                         PDFTextStripper stripper = new PDFTextStripper();
+                        stripper.setSortByPosition(false);
                         int totalPages = doc.getNumberOfPages();
 
                         for (int i = 1; i <= totalPages; i++) {
-                            if (isCancelled()) break;
+                            try {
+                                updateMessage("Scanning file " + fileCount + " of " + totalFiles + ": " + file.getName() + "\nPages scanned: " + pageCount + " / " + totalOverallPages);
+                                if (isCancelled()) break;
+                                pageCount++;
+                                stripper.setStartPage(i);
+                                stripper.setEndPage(i);
+                                String pageText = stripper.getText(doc);
 
-                            stripper.setStartPage(i);
-                            stripper.setEndPage(i);
-                            String pageText = stripper.getText(doc);
+                                if (pageText.toLowerCase().contains(searchQuery.toLowerCase())) {
+                                    final int page;
+                                    if (i > 0) {
+                                        page = i - 1; // Convert to zero-based index
+                                    } else {
+                                        page = i;
+                                    }
 
-                            if (pageText.toLowerCase().contains(searchQuery.toLowerCase())) {
-                                int page = i;
+                                    javafx.application.Platform.runLater(() -> {
+                                        call_back_function_for_matches.accept(new SearchResult(file, page));
+                                        // System.out.println("Found match on page " + page);
+
+                                    });
+                                }
+
+                                double filePercent = (double) i / totalPages;
+                                int pages = i;
                                 javafx.application.Platform.runLater(() -> {
-                                    call_back_function_for_matches.accept(new SearchResult(file, page));
-                                    System.out.println("Found match on page " + page);
-
+                                    statusLabel.setText("Page " + pages + " out of " + totalPages);
+                                    myProgressBar.setProgress(filePercent);
                                 });
+                            } catch (java.io.EOFException e) {
+                                System.err.println("\n\nReached unexpected end of file while reading page " + i + " of file: " + file.getAbsolutePath());
+                                // e.printStackTrace();
+                            } catch (Exception e) {
+                                System.err.println("\n\nError processing page " + i + " of file: " + file.getAbsolutePath());
+                                // e.printStackTrace();
                             }
-
-                            double filePercent = (double) i / totalPages;
-                            int pages = i;
-                            javafx.application.Platform.runLater(() -> {
-                                statusLabel.setText("Page " + pages + " out of " + totalPages);
-                                myProgressBar.setProgress(filePercent);
-                            });
                         }
+                        updateMessage("Scanning file " + fileCount + " of " + totalFiles + ": " + file.getName() + "\nPages scanned: " + pageCount + " / " + totalOverallPages);
                         doc.close();
+                    } catch (java.io.EOFException e) {
+                        System.err.println("\n\n");
+                        System.err.println("\n\nError: Reached unexpected end of file while reading: " + file.getAbsolutePath());
+                        // e.printStackTrace();
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        // This way we can try and figure out what broke
+                        System.err.println("\n\nError reading file: " + file.getAbsolutePath());
+                        // e.printStackTrace();
                     }
                 }
                 return null;
@@ -119,10 +170,12 @@ public class Main {
         });
 
         activeSearchTask.setOnSucceeded(e -> {
+            unbindProgress();
         statusLabel.setText("Search Complete!");
         });
 
         activeSearchTask.setOnFailed(e -> {
+            unbindProgress();
         overallStatusLabel.setText("Error during scan.");
         activeSearchTask.getException().printStackTrace();
         });
